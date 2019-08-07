24f2bb13740242217287e6307627a4716f9aeb64
Admin <nhattruongniit@yahoo.com.vn>
2019-08-07 23:13:21 +0700

    done code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useTranslation, Trans } from "react-i18next";
 
-function App() {
+export default function App() {
+  const { t: translate, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
+  const index = 11;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h2>{translate("Welcome to React")}</h2>
+        <button onClick={() => changeLanguage("de")}>de</button>
+        <button onClick={() => changeLanguage("en")}>en</button>
+      </div>
+      <div className="App-intro">
+        <Trans>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </Trans>
+        <Trans i18nKey="welcome">trans</Trans>
+        <Trans>{index + 1} xxx</Trans>
+      </div>
+      <div style={{ marginTop: 40 }}>
+        Learn more:&nbsp;
+        <a href="https://react.i18next.com">https://react.i18next.js</a>
+      </div>
     </div>
   );
 }
-
-export default App;
